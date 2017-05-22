aaa55bd8981475a12ba478dbabf11af0be3da742
Xavier Francisco <xavier.n.francisco@gmail.com>
2017-05-22 01:34:24 +0100

    proxy

--- proxyserver.py
+++ proxyserver.py
@@ -5,19 +5,33 @@ from threading import Thread
 import config
 
 
-class Server:
+class MonitorConnection:
     def __init__(self):
-        pass
+        self.recent_seq = []
+        self.conversation = {}
 
-    def add_probe_response(server, response):
-        pass
+    def probe_request(self, msg):
+        self.recent_seq.append(msg['seq'])
+        if (len(self.recent_seq) > 1000):
+            self.recent_seq = self.recent_seq[1:]
 
+        self.conversation[msg['seq']] = ((time.time(), msg), (None, None))
 
-def server_score(probe_response):
-    assert(probe_response['type'] == 'probe_response')
-    if probe_response['critical']:
-        return -1
-    if probe_response['']:
+    def probe_response(self, response):
+        (req, _) = self.conversation[response['seq']]
+        self.conversation[response['seq']] = (req, (time.time(), response))
+
+    def available(self):
+        last5 = self.recent_seq[-5:]
+
+        if all(time is None for (_, (time, _)) in last5):
+            return False
+        if any(msg is not None and msg['critical'] for (_, (_, msg)) in last5):
+            return False
+
+        return True
+
+    def __cmp__(self, y):
         pass
 
 
@@ -27,7 +41,7 @@ class ProxyServerMonitor:
         self.socket = socket.socket(socket.AF_INET,
                                     socket.SOCK_DGRAM)
         self.socket.bind(('', config.udp_port))
-        self.servers = set()
+        self.servers = {}
 
         t1 = Thread(target=self.listen)
         t1.start()
@@ -41,22 +55,27 @@ class ProxyServerMonitor:
             msg = json.loads(msg.decode())
 
             if msg['type'] == 'register':
-                self.servers.add(address)
+                self.servers[address] = MonitorConnection()
+                self.servers[address].probe_response(msg)
+
                 print("Register", address)
+            else:
+                self.servers[address].probe_response(msg)
 
     def shout(self):
         def shout_msg(seq):
             return {'type': 'probe_request',
-                    'seq': seq}
+                    'seq': seq,
+                    }
 
         sequence = 0
         while True:
             for server in self.servers:
+                msg = shout_msg(sequence)
+                server.probe_request(msg)
+                self.socket.sendto(json.dumps(msg.encode()), server)
 
-                msg = json.dumps(shout_msg(sequence)).encode()
-                self.socket.sendto(msg, server)
-
-            time.sleep(10)
+            time.sleep(5)
             sequence += 1
 
 
